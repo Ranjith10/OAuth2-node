1f8d2f390f6aadcd90ef179fb75caabb2fc2f184
Ranjith kumar <ranjit4072@gmail.com>
2020-07-07 12:52:43 +0530

    Added and sty;ed social login buttons

--- client/src/authentication/OAuth.js
+++ client/src/authentication/OAuth.js
@@ -5,7 +5,29 @@ import './OAuth.css'
 const OAuth = () => {
     return (
         <div className = 'OAuth-wrapper'>
-            ljhl
+            <div className = 'social-login-container' >
+                <div className = 'app-description'>
+                    <i className = 'fas fa-rocket' />
+                    OAuth 2.0 Demo
+                </div>
+                <div className = 'login-details-text'>
+                    Login using following to get quick access
+                </div>
+                <div className = 'social-login-wrapper'>
+                    <div className = 'social-login google'>
+                        <i className = 'fab fa-google-plus-g' />
+                        Signin with Google
+                    </div>
+                    <div className = 'social-login facebook'>
+                        <i className = 'fab fa-facebook-f' aria-hidden="true" />
+                        Signin with Facebook
+                    </div>
+                    <div className = 'social-login twitter'>
+                        <i className = 'fab fa-twitter' />
+                        Signin with Twitter
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
